5b347a6e6a6c9fdc4afe48a8f7d4c80217b9aaac
Pawel Raszewski <pawel.raszewski@teamaol.com>
2016-03-27 20:06:27 -0400

    go pi

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var http = require('http');  
 http.createServer(function (req, res) {  
       res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Cookie Monster!\n');
+        res.end('Cookie Monster! PAWEL TEST\n');
 }).listen(1337);
