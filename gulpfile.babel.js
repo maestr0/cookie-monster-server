adefe2408433e2658bcc2e31d7886c740456d2a6
Pawel Raszewski <raszewski@gmail.com>
2016-07-31 01:11:50 -0400

    gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,4 @@
-import gulb from "gulp"
+import gulp from "gulp"
 import shell from "gulp-shell"
 import rimraf from "rimraf"
 import run from "run-sequence"
@@ -8,6 +8,37 @@ import server from "gulp-live-server"
 const paths = {
   js: ['./src/**/*.js'],
   destination: './app'
-}
+};
 
-gulp.task('default')
+gulp.task('default', cb => {
+  run('server', 'build', 'watch', cb);
+});
+
+gulp.build('build', cb=> {
+  run('clean', 'flow', 'babel', 'restart', cb);
+});
+
+gulp.build('clean', cb=> {
+  rimraf(paths.destination, cb);
+});
+
+gulp.build('flow', shell.task(['flow']), {ignoreErrors: true});
+
+gulp.build('babel', shell.task(['babel src --out-dir app']));
+
+
+let express;
+
+gulp.task('server', ()=> {
+  express = server.new(paths.destination)
+});
+
+gulp.task('restart', ()=> {
+  express.start.bind(express)();
+});
+
+gulp.task('watch', ()=> {
+  return watch(paths.js, ()=> {
+    gulp.start('build');
+  });
+});
