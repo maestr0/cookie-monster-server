246ab7b6a9a31559faca4a45f39db14ec2353e2e
Pawel Raszewski <raszewski@gmail.com>
2016-07-31 20:37:48 -0400

    mcp support

--- src/temperature/index.js
+++ src/temperature/index.js
@@ -1,3 +1,17 @@
+import mcpadc from 'mcp-spi-adc'
+
+var tempSensor = mcpadc.open(0, {speedHz: 20000}, (err) => {
+  if (err) throw err;
+
+  setInterval(() => {
+    tempSensor.read((err, reading) => {
+      if (err) throw err;
+
+      console.log((reading.value * 3.3 - 0.5) * 100);
+    });
+  }, 1000);
+});
+
 export default () => {
   console.log("temp is 40C");
 }
