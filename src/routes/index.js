135ba5a735bfa7efac392dc880af8a5b5d92f1a8
Pawel Raszewski <raszewski@gmail.com>
2017-01-15 22:09:55 -0500

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Cookie Monster (ro)BOT' });
 });
 
 module.exports = router;
