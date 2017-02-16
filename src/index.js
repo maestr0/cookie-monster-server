8f17589cae6da6e2687b9c117df16c47142e07be
Pawel Raszewski <raszewski@gmail.com>
2017-02-15 23:38:08 -0500

    gpio

--- src/index.js
+++ src/index.js
@@ -15,4 +15,15 @@ expressSetup(app);
 
 slack();
 
+
+var gpio = require('rpi-gpio');
+
+gpio.on('change', function(channel, value) {
+    logger.info('Channel ' + channel + ' value is now ' + value);
+});
+gpio.setup(23, gpio.DIR_IN, gpio.EDGE_BOTH);
+gpio.setup(24, gpio.DIR_IN, gpio.EDGE_BOTH);
+
+module.exports = gpio
+
 logger.info('CM started');
